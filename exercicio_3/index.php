f75beb77a4c2812f0ce7981d5887f0d4510eacbc
lucaszstrr <lucaaszstr@gmail.com>
2025-02-19 11:51:10 -0300

    Supermercado, fazer algumas alterações

--- exercicio_3/index.php
+++ exercicio_3/index.php
@@ -5,7 +5,7 @@
     $produto1 = new Produto('maçã', 4, 2, 10, 'dinheiro');
 
     echo "Foram vendidas " . $produto1->quantidade . " unidades de " . $produto1->produto . PHP_EOL;
-    echo "Valor da compra: R$" . $produto1->getCalculoPreco() . PHP_EOL;
-    echo "Agora o estoque é de ". $produto1->getCalculoEstoque() . " " . $this->produto . PHP_EOL;
+    echo "Valor da compra: R$" . $produto1->CalculoPreco() . PHP_EOL;
+    echo "Agora o estoque é de ". $produto1->CalculoEstoque() . PHP_EOL;
     echo "O meio de pagamento utilizado foi " . $produto1->pagamento . PHP_EOL;
     
